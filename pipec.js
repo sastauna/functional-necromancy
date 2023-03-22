5b813d78233b186463678112ea72886cbb823ea2
coalpha <22552895+coalpha@users.noreply.github.com>
2023-03-22 15:16:14 -0700

    pipec 30 chr reduction

--- pipec.js
+++ pipec.js
@@ -1,1 +1,6 @@
+// pipec aka pipes
+// original 2018
 pipec=(_=>_=((P,A=Array.isArray)=>(...f)=>(...a)=>f.reduce((b,w)=>(A(w)?w.map(v=>(A(v)?_(...v)(...b):P(b,v))):[P(b,w)]),a)[0])((a,v)=>(~~v==v?a[v]:v(...a))))()
+// revised 2023
+pipec=(P=>P=(...f)=>(...a)=>f.reduce((p,w)=>(Array.isArray(w)?w:[w]).map(F=>Array.isArray(F)?P(...F)(...p):~~F==F?p[F]:F(...p)),a)[0])()
+pipec=((A,P)=>P=(...f)=>(...a)=>f.reduce((p,w)=>(A(w)?w:[w]).map(F=>A(F)?P(...F)(...p):~~F==F?p[F]:F(...p)),a)[0])(Array.isArray)
